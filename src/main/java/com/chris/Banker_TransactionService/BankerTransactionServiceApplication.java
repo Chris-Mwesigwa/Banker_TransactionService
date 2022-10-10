b91d2948663a0deda842f0495fee740a6d2acf17
chris-mwesigwa <cmwesigwa@aetotechs.com>
2022-10-10 15:29:12 +0300

    inital commit

---
+++ src/main/java/com/chris/Banker_TransactionService/BankerTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_TransactionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankerTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankerTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/Banker_TransactionService/BankerTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_TransactionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
